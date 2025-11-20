de90e99e88bd902c2cc833a402d059af4ba56c42
mogera551 <mogera551@coo-d.jp>
2025-11-21 07:43:45 +0900

    1.3.7

---
+++ packages/core/dist/EasyLoaders/components--disable-shadow-dom--locale-de.js
@@ -0,0 +1,8 @@
+import { bootstrapStructive, config } from "../structive.js";
+
+config.autoLoadFromImportMap = true;
+config.enableMainWrapper = false;
+config.enableRouter = false;
+config.shadowDomMode = "none";
+config.locale = "de";
+bootstrapStructive();
