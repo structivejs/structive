6129657a5a9a42e130e4979021e5a8909c9d4d5e
mogera551 <mogera551@coo-d.jp>
2025-11-30 07:46:01 +0900

    chore: update version to 1.4.1 in package.json and package-lock.json; remove unused isVoidElement.js

--- packages/core/dist/ComponentEngine/isVoidElement.js
+++
@@ -1,8 +0,0 @@
-const voidElements = new Set([
-    "area", "base", "br", "col", "embed", "hr",
-    "img", "input", "link", "meta", "source", "track", "wbr"
-]);
-export function isVoidElement(tagName) {
-    // タグ名を小文字に変換して、void要素のセットに存在するかを確認
-    return voidElements.has(tagName.toLowerCase());
-}
